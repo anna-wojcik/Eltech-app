3f9216fce9c1ad0643467bfd199a9cb4de9a2dc1
Anna Wójcik <aniawojcik894@gmail.com>
2025-05-30 18:51:20 +0200

    iframe attributes changed to camelCase

--- src/features/contactPage/ContactPage.js
+++ src/features/contactPage/ContactPage.js
@@ -30,7 +30,7 @@ export default () => {
             <StyledSection>
                 <Title>Zobacz na mapie</Title>
                 <MapContainer>
-                    <StyledIframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2497.580945565326!2d22.557013776147546!3d51.24521212982375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722576931d3e58f%3A0x81802a67fdf4d6de!2sPrezydenta%20Gabriela%20Narutowicza%2018%2F3%2C%2020-400%20Lublin!5e0!3m2!1spl!2spl!4v1748617436223!5m2!1spl!2spl"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></StyledIframe>
+                    <StyledIframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2497.580945565326!2d22.557013776147546!3d51.24521212982375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722576931d3e58f%3A0x81802a67fdf4d6de!2sPrezydenta%20Gabriela%20Narutowicza%2018%2F3%2C%2020-400%20Lublin!5e0!3m2!1spl!2spl!4v1748617436223!5m2!1spl!2spl"  allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></StyledIframe>
                 </MapContainer>
             </StyledSection>
         </>
